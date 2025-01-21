959ac7ff27e3cbca1e672f22c6fcaefe09c995b1
abhishekjaiswal07890 <abhishekjaiswal07890@gmail.com>
2025-01-21 14:27:31 +0000

    main page done

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
-
+  images: {
+            domains: ['images.unsplash.com', 'res.cloudinary.com']
+                }
+  
 };
 
 export default nextConfig;
